93e83ff423d51960ddb0ab42f8b94469b8e24b97
Alex Pendleton <alexpendleton@Alexs-MacBook-Pro.local>
2015-09-09 23:29:14 -0400

    Got the UI looking (pretty much) how I want it for the login screen. There's now validation for this view, using a protocol for a login service. There's also a library to deal with the keyboard sliding up over forms thing.

---
+++ MyFavoriteMovies/MyFavoriteMovies/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  MyFavoriteMovies
+//
+//  Created by Alex Pendleton on 9/9/15.
+//  Copyright (c) 2015 Alex Pendleton. All rights reserved.
+//
+
+import Foundation
